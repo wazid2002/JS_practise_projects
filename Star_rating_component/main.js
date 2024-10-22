c76de20f3bfe058c9a9300e8cbdb20c754c08f2f
wazid2002 <munavalliwazid@gmail.com>
2024-10-22 23:36:13 +0530

    star rating component completed

--- Star_rating_component/main.js
+++ Star_rating_component/main.js
@@ -1,11 +1,45 @@
-const stars=documented.querySelectorAll('.fa-star');
+const stars=document.querySelectorAll('.fa-star-o');
 const selectedRatingValueText=document.querySelector('.Selected-rating');
-
 let currentTotalSelectedStars=-1;
 
 stars.forEach((starItem,index)=>{
     starItem.dataset.rating=index +1;
-    starItem.addEventListener('mouseover','handleMouseOver')
-    starItem.addEventListener('click','handleclick')
-})
+    starItem.addEventListener('mouseover',handleMouseOver);
+    starItem.addEventListener('click',handleClick);
+    starItem.addEventListener('mouseleave',handleMouseLeave);
+})  
+
+function handleMouseOver(event){
+    currentTotalSelectedStars=event.target.dataset.rating;
+    if(!currentTotalSelectedStars) return
+    else handleUpdateRatingState(currentTotalSelectedStars)
+
+
+}
+
+function handleUpdateRatingState(getCurrentratingValue){
+    for(let i=0;i<5;i++){
+        if(i<getCurrentratingValue){
+            stars[i].classList.replace("fa-star-o","fa-star");
+        }
+        else{
+            stars[i].classList.replace("fa-star","fa-star-o");
+        }
+    }
+}
+
+function handleClick(event) {
+    const currentTotalSelectedStars=event.target.dataset.rating;
+    handleUpdateRatingState(currentTotalSelectedStars);
+    selectedRatingValueText.textContent=currentTotalSelectedStars;
+
+    
+}
+
+function handleMouseLeave() {
+    handleUpdateRatingState(currentTotalSelectedStars);
+
+    
+}
+
 
