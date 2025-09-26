40b795c8d628c9f9c69827e3fa6fe85e034e3ee5
Wazid <munavalliwazid@gmail.com>
2025-09-26 20:46:54 +0530

    add React-Projects

---
+++ React Projects/ToDo-app/src/components/TodoInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TodoInput() {
+  return (
+    <form action="">
+        <input type='text' placeholder='Enter the Task'></input>
+    </form>
+  )
+}
+
+export default TodoInput
